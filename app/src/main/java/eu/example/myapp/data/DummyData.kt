3bc825b13c5dee6886def7c16e604099e1b7d702
Raa-14 <hmira992@gmail.com>
2024-11-14 23:18:56 +0700

    Push Pertama

---
+++ app/src/main/java/eu/example/myapp/data/DummyData.kt
@@ -0,0 +1,69 @@
+package eu.example.myapp.data
+
+import eu.example.myapp.R
+
+object DummyData {
+    val skincareList = listOf(
+        Brand(
+            id = 1,
+            name = "kucing",
+            photoProduct = R.drawable.kucing,
+            price = 1
+
+        ),
+        Brand(
+            id = 2,
+            name = "ikan",
+            photoProduct = R.drawable.ikan,
+            price = 1
+        ),
+        Brand(
+            id = 3,
+            name = "beruang",
+            photoProduct = R.drawable.beruang,
+            price = 100
+        ),
+        Brand(
+            id = 4,
+            name = "koala",
+            photoProduct = R.drawable.koala,
+            price = 10
+        ),
+        Brand(
+            id = 5,
+            name = "bebek",
+            photoProduct = R.drawable.bebek,
+            price = 10
+        ),
+        Brand(
+            id = 6,
+            name = "kutu",
+            photoProduct = R.drawable.kutu,
+            price = 10
+        ),
+        Brand(
+            id = 7,
+            name = "macan",
+            photoProduct = R.drawable.ikan,
+            price = 10
+        ),
+        Brand(
+            id = 8,
+            name = "jerapah",
+            photoProduct = R.drawable.jerapah,
+            price = 10
+        ),
+        Brand(
+            id = 9,
+            name = "kura",
+            photoProduct = R.drawable.kura,
+            price = 10
+        ),
+        Brand(
+            id = 10,
+            name = "kutu",
+            photoProduct = R.drawable.kutu,
+            price = 10
+        ),
+    )
+}
